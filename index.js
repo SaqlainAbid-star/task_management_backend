deaa911ed9085469e35d559e4664e5a54dee070c
Saqlain Abid <jamalisaqlain7@gmail.com>
2023-11-30 14:38:12 +0500

    Db updated

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(bodyParser.json({limit: '30mb',extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb',extended: true}));
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Sever Running");
+  });
+
 app.use("/tasks", tasks);
 app.use("/users", users);
 app.use("/groups", groups);
--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://127.0.0.1:27017/task-management'
+// const url = 'mongodb://127.0.0.1:27017/task-management'
+const url = 'mongodb+srv://Saqlain:Saqlain123@cluster0.oo31c.mongodb.net/?retryWrites=true&w=majority';
+
 
 mongoose.connect(url).then(()=>{
     console.log("DB Connected");
