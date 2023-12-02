7719060aa658aace9ab53997b7d4635def2f0878
Saqlain Abid <jamalisaqlain7@gmail.com>
2023-12-02 10:36:35 +0500

    cors updated 4

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
-const app = express();
+const path = require("path");
+var cors = require('cors')
+const app = express()
 const tasks = require("./routes/tasks");
 const users = require("./routes/users");
 const groups = require("./routes/groups");
-const uploadImage = require("./routes/uploadImage");
+const uploadImage = require("./routes/uploadImage")
 const mongoose = require("./models/connection");
 
 
-// Allow requests from all origins (you can replace '*' with your specific frontend URL)
-app.use(cors());
+const corsOptions ={
+    origin: 'http://localhost:3000',
+    credentials:true,            //access-control-allow-credentials:true
+}
+app.use(cors(corsOptions));
 
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
 
 
 // sending some images large in size
 // setting up the body parser to properly send our requests
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({limit: '30mb',extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb',extended: true}));
 
 app.get("/", (req, res) => {
-  res.send("Server Running");
-});
+    res.send("Sever Running");
+  });
 
 app.use("/tasks", tasks);
 app.use("/users", users);
 app.use("/groups", groups);
-app.use("/", uploadImage);
+app.use("/",uploadImage)
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
-});
-
-
-
+app.listen(5000);
