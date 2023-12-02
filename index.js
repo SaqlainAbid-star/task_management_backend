637d956f071782671bcb3aef76cbb254a667db44
Saqlain Abid <jamalisaqlain7@gmail.com>
2023-12-02 15:05:05 +0500

    cors updated 14

--- index.js
+++ index.js
@@ -19,35 +19,38 @@ const corsOptions = {
 
 app.use(express.json());
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
-app.use(bodyParser.json());
-
 app.use(cors(corsOptions)); // use cors middleware for all routes
 
+app.use(express.urlencoded({
+  extended: true,
+}))
 
 
+
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
+
+// app.use(bodyParser.json());
 
 
 
 // sending some images large in size
 // setting up the body parser to properly send our requests
-app.use(bodyParser.json({limit: '30mb',extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb',extended: true}));
+// app.use(bodyParser.json({limit: '30mb',extended: true}));
+// app.use(bodyParser.urlencoded({limit: '30mb',extended: true}));
 
 app.get("/", (req, res) => {
-    res.send("Sever Running");
-  });
+  res.send("Sever Running");
+});
 
 app.use("/tasks", tasks);
 app.use("/users", users);
 app.use("/groups", groups);
-app.use("/",uploadImage)
+app.use("/", uploadImage)
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
   console.log("Your server is running on 5000");
 });
