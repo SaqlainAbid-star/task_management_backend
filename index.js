5ccb92381f25e39dff62e5fa51f782677ad6324f
Saqlain Abid <jamalisaqlain7@gmail.com>
2023-12-02 10:26:48 +0500

    cors updated 3

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 const cors = require('cors');
 const app = express();
 const tasks = require("./routes/tasks");
